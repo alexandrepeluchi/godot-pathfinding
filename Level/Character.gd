6db790098eeece0060ed9166e181ba61c00639c9
Alexandre Peluchi <alexandrepeluchi@gmail.com>
2021-01-15 23:18:45 -0300

    Movimentação do personagem baseado nas coordenadas a serem percorridas

--- Level/Character.gd
+++ Level/Character.gd
@@ -9,12 +9,32 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
-	pass
+	var move_distance := speed * delta
+	move_along_path(move_distance)
 
 
 func move_along_path(distance : float) -> void:
-	pass
+	var start_point := position
+	
+	for index in range(path.size()):
+		var distance_to_next := start_point.distance_to(path[0])
+		
+		if distance <= distance_to_next and distance >= 0.0:
+			position = start_point.linear_interpolate(path[0], distance / distance_to_next)
+			break
+		elif distance < 0.0:
+			position = path[0]
+			set_process(false)
+			break
 
+		distance -= distance_to_next
+		start_point = path[0]
+		path.remove(0)
 
 func set_path(value : PoolVector2Array) -> void:
-	pass
+	path = value
+	
+	if value.size() == 0:
+		return
+		
+	set_process(true)
